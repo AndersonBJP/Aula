ff937e865c438367c9bcc306c4ba719a1dfb93f2
AndersonBJP <andersonbarbosailha@gmail.com>
2024-06-13 20:39:22 -0300

    First Commit

---
+++ novo1.py
@@ -0,0 +1,10 @@
+"""
+A cliente Paula Martins realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% porcento na sua próxima compra
+
+"""
+
+nome = "Paula Martins"
+saldo = 500
+desconto = 10
+
+print("A cliente" , nome , "realizou uma compra no valor de" , saldo , "e ganhou um desconto de" , desconto , "na sua próxima compra")
